1c02b005f3916640b6b90957614deda6ead04218
JongMin Kim <kjm0202@empas.com>
2025-04-05 03:30:13 +0900

    refactor: update import statement

--- lib/pwa_update_listener.dart
+++ lib/pwa_update_listener.dart
@@ -1,2 +1,2 @@
 export 'package:pwa_update_listener/src/fake_update_listener.dart'
-    if (dart.library.html) 'package:pwa_update_listener/src/real_update_listener.dart';
+    if (dart.library.js_interop) 'package:pwa_update_listener/src/real_update_listener.dart';
